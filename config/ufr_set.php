cfcb564e065156b2eb55bd7990e49f9861a8b613
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-01-14 13:19:41 +0000

    ajout des ues

--- config/ufr_set.php
+++ config/ufr_set.php
@@ -15,27 +15,33 @@ return [
     ['code'=>'humanites_et_entreprises','nom'=>'humanites_et_entreprises','ecs'=>['anglais_2','technique_de_communication']],
   ],
   'l2' => [
-    ['code'=>'algorithmique','nom'=>'algorithmique','ecs'=>['cours','td','tp']],
-    ['code'=>'programmation','nom'=>'programmation','ecs'=>['cours','td','tp']]
+    ['code'=>'algorithmique','nom'=>'algorithmique','ecs'=>['algorithme_et_programmation_2','architecture_des_ordinateurs']],
+    ['code'=>'mathematique','nom'=>'mathematique','ecs'=>['probabilite_statistique','calcul_numerique']],
+    ['code'=>'reseau_et_systeme','nom'=>'reseau_et_systeme','ecs'=>['systeme_exploitation','introduction_aux_reseaux']],
+     ['code'=>'systeme_information','nom'=>'systeme_information','ecs'=>['analyse_et_conception','introduction_aux_base_de_donnees']],
+    ['code'=>'humanites_et_entreprises','nom'=>'humanites_et_entreprises','ecs'=>['projet_personnel','anglasi_3']],
   ],
   'l3' => [
-    ['code'=>'algorithmique','nom'=>'algorithmique','ecs'=>['cours','td','tp']],
-    ['code'=>'programmation','nom'=>'programmation','ecs'=>['cours','td','tp']]
+    ['code'=>'programmation','nom'=>'programmation','ecs'=>['programmation_mobile','programmation_orientee_objet']],
+    ['code'=>'genie_logiciel','nom'=>'genie_logiciel','ecs'=>['developpement_web','introduction_au_genie_logiciel']],
+    ['code'=>'informatique','nom'=>'informatique','ecs'=>['base_de_donnees_avancee','programmation_fonctionelle']],
+     ['code'=>'systeme_information','nom'=>'systeme_information','ecs'=>['analyse_et_conception','introduction_aux_base_de_donnees']],
+    ['code'=>'humanites_et_entreprises','nom'=>'humanites_et_entreprises','ecs'=>['creation_entreprise','anglasi_5']],
   ]
  ],
  'physique_chimie' => [
   'l1' => [
-    ['code'=>'physique_generale','nom'=>'Physique Générale','ecs'=>['cours','tp']]
+    ['code'=>'physique_generale','nom'=>'physique_generale','ecs'=>['cours','tp']]
   ]
  ],
  'sciences_eaux_env' => [
   'l1' => [
-    ['code'=>'hydrologie','nom'=>'Hydrologie','ecs'=>['cours','tp']]
+    ['code'=>'hydrologie','nom'=>'hydrologie','ecs'=>['cours','tp']]
   ]
  ],
  'math_info' => [
   'l1' => [
-    ['code'=>'analyse','nom'=>'Analyse','ecs'=>['cours','td']]
+    ['code'=>'analyse','nom'=>'analyse','ecs'=>['cours','td']]
   ]
  ]
 ];
